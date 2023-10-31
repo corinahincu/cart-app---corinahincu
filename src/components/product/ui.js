888b4c63e5a719994037331b799829d40d14eeee
corinahincu <117738625+corinahincu@users.noreply.github.com>
2023-10-31 17:51:12 +0100

    Add files via upload

--- src/components/product/ui.js
+++ src/components/product/ui.js
@@ -3,7 +3,7 @@ export function Product({ name, image }) {
   return (
     <div className="product">
       <h3>{name}</h3>
-      <img src={image} alt={name} style={{ width: "100px" }} />
+      <img src={image} alt={name} style={{ width: "200px" }} />
     </div>
   );
 }
